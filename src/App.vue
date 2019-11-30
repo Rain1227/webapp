130ea572930f643a0a75ae47aa66a394477664f2
Rain1227 <1262208607@qq.com>
2019-11-30 18:00:23 +0800

    项目初始化-调整目录结构

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'APP',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  methods: {}
+}
+</script>
+
+<style scooped>
+
+</style>
