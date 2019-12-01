361e8d6ccb6aa464c3e4fb88c8a2e0a3adb2ebe6
Rain1227 <1262208607@qq.com>
2019-12-01 20:31:50 +0800

    登录页面完成

---
+++ src/api/user.js
@@ -0,0 +1,12 @@
+
+import request from '@/utils/request'
+// 用户相关的请求模块
+
+// 用户登录
+export function login (data) {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/authorizations',
+    data
+  })
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,11 +7,13 @@
     <!-- 表单 -->
     <van-cell-group>
       <van-field
+      v-model="user.mobile"
         label="手机号"
         placeholder="请输入手机号"
       />
 
       <van-field
+      v-model="user.code"
         label="验证码"
         placeholder="请输入验证码"
       />
@@ -20,26 +22,56 @@
 
     <!-- 登录按钮 -->
     <div class="login-btn-box">
-      <van-button type="info">登录</van-button>
+      <van-button type="info" @click="onLogin">登录</van-button>
     </div>
     <!-- /登录按钮 -->
   </div>
 </template>
 
 <script>
+// 引入封装的axios模块
+import { login } from '@/api/user'
 export default {
   name: 'LoginPage',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '13911111111',
+        code: '246810'
+      }
+    }
   },
   computed: {},
   watch: {
   },
   created () {},
-  methods: {}
+  methods: {
+    async onLogin () {
+      // 动态更新提示
+      this.$toast.loading({
+        duration: 0, // 持续展示 toast
+        forbidClick: true,
+        message: '登录中'
+      })
+      try {
+        const res = await login(this.user)
+        // const res = await request({
+        //   method: 'POST',
+        //   url: '/app/v1_0/authorizations',
+        //   data: this.user
+        // })
+        console.log('登录成功', res)
+        this.$toast.success('登录成功')
+      } catch (err) {
+        console.log('登录失败', err)
+        this.$toast.fail('出错了')
+      }
+    }
+  }
 }
+
 </script>
 
 <style scoped lang="less">
